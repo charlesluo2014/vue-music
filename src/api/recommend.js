bc67b6a09ee40c661e15f86ab63755b5352f0c2d
charlesluo2014 <lishu.luo@foxmail.com>
2017-10-02 11:32:32 +0800

    add

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -40,7 +40,7 @@ export function getDiscList() {
 }
 
 export function getSongList(disstid) {
-  const url = '/api/getSongList'
+  const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
 
   const data = Object.assign({}, {
     type: 1,
@@ -57,11 +57,7 @@ export function getSongList(disstid) {
     notice: 0,
     platform: 'yqq',
     needNewCode: 0
-  });
+  })
 
-  return axios.get(url, {
-    params: data
-  }).then(res => {
-    return Promise.resolve(res.data)
-  });
+  return jsonp(url, data, options1)
 }
